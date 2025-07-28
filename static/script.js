b83b43539411bd74afc3b9d458ceab888b37aa98
JSCatLower <kalmykovyaroslavkalmykov@yandex.ru>
2025-07-28 14:21:07 +0400

    Добавил кнопку меняющую фон и обновил цвет текста

--- static/script.js
+++ static/script.js
@@ -2,4 +2,8 @@ function waddle() {
   console.log('Готов к работе! 🐧');
   alert('Пингвин доволен твоим JS!');
 }
+function toggleTheme() {  
+    document.body.style.background = document.body.style.background === 'black' ? 'white' : 'black';  
+    document.body.style.color = document.body.style.color === 'lime' ? 'black' : 'lime';  
+}  
 
